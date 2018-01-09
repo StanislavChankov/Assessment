4549a73f5c3de5ca223741738f95990fc7003ccc
Stanislav Chankov <stanislavchankov94@gmail.com>
2018-01-09 21:40:21 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = 'mitakes pederakes';
+  titlichka: string = 'mitakes pederakes';
 }
